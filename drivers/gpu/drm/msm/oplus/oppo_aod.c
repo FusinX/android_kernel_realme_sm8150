f70d249a7910bdf273d4126f520a5d8dbab221cb
jabashque <jabashque@gmail.com>
2023-01-17 23:17:54 -0800

    drm: oplus: Set AoD low light mode as default
    The brightness level of AoD high mode is a bit too much for an always-on
    display. Default to low mode instead to help reduce screen burn-in.
    
    Change-Id: I5aa01237bcd000fe36d1430bb3a41592d08e91f4

--- drivers/gpu/drm/msm/oplus/oppo_aod.c
+++ drivers/gpu/drm/msm/oplus/oppo_aod.c
@@ -15,7 +15,7 @@
 #include "oppo_aod.h"
 #include <drm/drm_mipi_dsi.h>
 
-int aod_light_mode = 0;
+int aod_light_mode = 1;
 DEFINE_MUTEX(oppo_aod_light_mode_lock);
 
 int __oppo_display_set_aod_light_mode(int mode) {
